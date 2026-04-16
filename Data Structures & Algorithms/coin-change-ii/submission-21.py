1d3cea362605c15962182c7a0644aeca6ed3affa
Harbinr1 <harbin.rashitii@gmail.com>
2026-04-16 18:47:37 +0200

    Add: coin-change-ii - submission-21

---
+++ Data Structures & Algorithms/coin-change-ii/submission-21.py
@@ -0,0 +1,23 @@
+class Solution:
+    def change(self, amount: int, coins: List[int]) -> int:
+        dp = [[-1 for j in range (amount +1)] for i in range(len(coins)) ]
+        def dfs(i,rem):
+            if rem == 0:
+                return 1
+            
+            if rem < 0 or i >= len(coins):
+                return 0
+            
+            if dp[i][rem] != -1:
+                return dp[i][rem]
+
+            take = dfs(i,rem - coins[i])
+            skip = dfs(i+1,rem)
+            dp[i][rem] = take + skip
+            return dp[i][rem]
+        
+        return dfs(0,amount)
+
+
+
+        

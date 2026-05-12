fecd09b173882cc2a99ebffdf7ee0937fa06fc25
Harbinr1 <harbin.rashitii@gmail.com>
2026-05-12 09:32:35 +0200

    Add: reconstruct-flight-path - submission-33

---
+++ Data Structures & Algorithms/reconstruct-flight-path/submission-33.py
@@ -0,0 +1,25 @@
+class Solution:
+    def findItinerary(self, tickets: List[List[str]]) -> List[str]:
+        stack = []
+        stack = [("JFK")]
+        graph = defaultdict(list)
+        res = []
+        minHeap = []
+        for s,t in tickets:
+            graph[s].append(t)
+        
+        for flight in graph:
+            heapq.heapify(graph[flight])
+        
+
+        while stack :
+            curr = stack[-1]
+            heapq.heapify(graph[curr])
+            if graph[curr]:
+                next_flight = heapq.heappop(graph[curr])
+                stack.append(next_flight)
+            else:
+                res.append(stack.pop())
+        return res[::-1]
+
+        

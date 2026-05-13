fbe8a83c2512feda9a8833172af69a4d8e38df8f
Harbinr1 <harbin.rashitii@gmail.com>
2026-05-13 17:37:18 +0200

    Add: hand-of-straights - submission-18

---
+++ Data Structures & Algorithms/hand-of-straights/submission-18.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isNStraightHand(self, hand: List[int], groupSize: int) -> bool:
+        freqCard = defaultdict(int)
+
+        for card in hand:
+            freqCard[card] += 1
+        
+
+        for card in sorted(freqCard.keys()):
+            while freqCard[card] > 0:
+                for i in range(groupSize):
+                    next_card = i + card
+                    if freqCard[next_card] == 0:
+                        return False
+                    else:
+                        freqCard[next_card] -= 1
+        return True

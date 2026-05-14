5c6010a8362247117a2f2c6b567b7932c1538ded
Harbinr1 <harbin.rashitii@gmail.com>
2026-05-14 09:00:31 +0200

    Add: foreign-dictionary - submission-13

---
+++ Data Structures & Algorithms/foreign-dictionary/submission-13.py
@@ -0,0 +1,48 @@
+class Solution:
+    def foreignDictionary(self, words: List[str]) -> str:
+        visited = set()
+        graph = defaultdict(list)
+
+        for i in range(len(words)-1):
+            w1 = words[i]
+            w2 = words[i+1]
+
+            j = 0
+            while j < len(w1) and j < len(w2):
+                if w1[j] != w2[j]:
+                    graph[w1[j]].append(w2[j])
+                    break
+                j += 1
+            if j == len(w2) and len(w1) > len(w2):
+                return ""
+                
+        processing = set()
+        res = []
+        def dfs(i):
+            if i in visited:
+                return True
+            
+            if i in processing:
+                return False
+
+            processing.add(i)
+            for nei in graph[i]:
+                if not dfs(nei):
+                    return False
+            processing.remove(i)
+            visited.add(i)
+            res.append(i)
+            return True
+        
+        unique = set()
+
+        for char in words:
+            for c in char:
+                if c not in unique:
+                    unique.add(c)
+                    if not dfs(c):
+                        return ""
+                    
+        
+        return "".join(res[::-1])
+        

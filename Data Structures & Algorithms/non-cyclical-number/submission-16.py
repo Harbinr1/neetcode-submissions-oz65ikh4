ef84f4b7adc3c5ab304ef994ca5b4a32235ad056
Harbinr1 <harbin.rashitii@gmail.com>
2026-05-16 15:54:07 +0200

    Add: non-cyclical-number - submission-16

---
+++ Data Structures & Algorithms/non-cyclical-number/submission-16.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        visited = set()
+
+        while n  not in visited:
+            visited.add(n)
+            total_sum = 0
+            while n > 0:
+                last_digit = n % 10
+                total_sum += last_digit * last_digit
+                n =n // 10
+            
+            n = total_sum
+        return n == 1

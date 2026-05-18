895b354f158013ee5e17a13f346079c0198ecd44
Harbinr1 <harbin.rashitii@gmail.com>
2026-05-18 11:44:46 +0200

    Add: multiply-strings - submission-10

---
+++ Data Structures & Algorithms/multiply-strings/submission-10.py
@@ -0,0 +1,29 @@
+class Solution:
+    def multiply(self, num1: str, num2: str) -> str:
+        digit_map = {'0':0,'1':1,'2':2,'3':3,'4':4,'5':5,'6':6,'7':7,'8':8,'9':9}
+        
+        val1 = 0
+        for char in num1:
+            val1 = val1 * 10 + digit_map[char]
+        
+        val2 = 0
+        for char in num2:
+            val2 = val2 * 10 + digit_map[char]
+        
+        total = val1*val2
+        res = []
+        if total == 0:
+            return "0"
+        while total > 0:
+            digit = total % 10
+            char = chr(digit + ord('0'))
+            res.append(char)
+            total = total // 10
+        
+        res_str = "".join(res[::-1])
+        return res_str
+        
+
+        
+
+        

57b8dec351ddf0b7cea35519c42a21a9f08790af
Harbinr1 <harbin.rashitii@gmail.com>
2026-05-19 06:31:12 +0200

    Add: count-squares - submission-18

---
+++ Data Structures & Algorithms/count-squares/submission-18.py
@@ -0,0 +1,16 @@
+class CountSquares:
+
+    def __init__(self):
+        self.map = defaultdict(int)
+        
+    def add(self, point: List[int]) -> None:
+        self.map[tuple(point)] += 1
+
+    def count(self, point: List[int]) -> int:
+        res = 0
+        px,py = point
+        for (x,y) ,cnt in list(self.map.items()):
+            if (abs(x - px) != abs(py-y)) or x == px or py == y:
+                continue
+            res += cnt * self.map[(x,py)] * self.map[(px,y)]
+        return res

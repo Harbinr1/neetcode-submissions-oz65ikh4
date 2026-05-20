058610023a6adeb18ab3236d12c86930f6b19de6
Harbinr1 <harbin.rashitii@gmail.com>
2026-05-20 15:18:32 +0200

    Add: sum-of-two-integers - submission-23

---
+++ Data Structures & Algorithms/sum-of-two-integers/submission-23.py
@@ -0,0 +1,12 @@
+class Solution:
+    def getSum(self, a: int, b: int) -> int:
+        carry = 0
+        while b != 0:
+            old_a = ((a ^ b) & 0xFFFFFFFF)
+            b = (a & b) << 1 & 0xFFFFFFFF
+
+            a = old_a
+        
+        if a >= 2 ** 31:
+            a-= 2**32
+        return a

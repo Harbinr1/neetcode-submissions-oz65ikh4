6c66dfd6e0294968bc4936b92b74f1494d70f12e
Harbinr1 <harbin.rashitii@gmail.com>
2026-05-23 14:45:52 +0200

    Add: duplicate-integer - submission-16

---
+++ Data Structures & Algorithms/duplicate-integer/submission-16.py
@@ -0,0 +1,11 @@
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        visited = set()
+
+        for n in nums:
+            if n not in visited:
+                visited.add(n)
+            else:
+                return True
+        return False
+        

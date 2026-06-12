25c376ef70266d19c2455ba157d905b7662fb158
Harbinr1 <harbin.rashitii@gmail.com>
2026-06-12 16:11:12 +0200

    Add: minimum-stack - submission-6

---
+++ Data Structures & Algorithms/minimum-stack/submission-6.py
@@ -0,0 +1,27 @@
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+        
+
+    def push(self, val: int) -> None:
+        if not self.stack:
+            min_so_far = val
+        else:
+            min_so_far = min(val,self.stack[-1][1])
+        self.stack.append((val,min_so_far))
+        
+
+        
+
+    def pop(self) -> None:
+        self.stack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1][0]
+
+        
+
+    def getMin(self) -> int:
+        return self.stack[-1][1]
+        

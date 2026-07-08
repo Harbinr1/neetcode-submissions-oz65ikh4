a80a8541f237ecb5773478b2bebcac8c4735ebaa
Harbinr1 <harbin.rashitii@gmail.com>
2026-07-08 15:44:10 +0200

    Add: combination-target-sum - submission-2

---
+++ Data Structures & Algorithms/combination-target-sum/submission-2.py
@@ -0,0 +1,24 @@
+class Solution:
+    def combinationSum(self, nums: List[int], target: int) -> List[List[int]]:
+        result = []
+        substack = []
+
+        def dfs(i,target):
+            if target == 0:
+                result.append(substack[:])
+                return
+            if target < 0:
+                return
+            
+            if i >= len(nums):
+                return
+            
+            substack.append(nums[i])
+            dfs(i,target-nums[i])
+            substack.pop()
+            dfs(i+1,target)
+        
+        dfs(0,target)
+        return result
+
+        

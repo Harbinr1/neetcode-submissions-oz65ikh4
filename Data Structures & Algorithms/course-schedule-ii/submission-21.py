c9f6e669507186514b057c28f27a94670d31c7ed
Harbinr1 <harbin.rashitii@gmail.com>
2026-07-18 16:10:13 +0200

    Add: course-schedule-ii - submission-21

---
+++ Data Structures & Algorithms/course-schedule-ii/submission-21.py
@@ -0,0 +1,34 @@
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        visited = set()
+        exploring = set()
+
+
+        mapEdges = defaultdict(list)
+
+        for a,b in prerequisites:
+            mapEdges[b].append(a)
+        
+
+        
+        def dfs(node):
+            if node in visited:
+                return True
+            if node in exploring:
+                return False
+            exploring.add(node)
+
+            for nei in mapEdges[node]:
+                if not dfs(nei):
+                    return False
+            exploring.remove(node)
+            visited.add(node)
+            result.append(node)
+            return True
+        result = []
+        for n in range(numCourses):
+            if not dfs(n):
+                return []
+        return result[::-1]
+        
+        

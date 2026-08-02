9623efecf90ed926cdb23ae83797a757ac48eede
Harbinr1 <harbin.rashitii@gmail.com>
2026-08-02 19:27:19 +0200

    Add: generate-parentheses - submission-30

---
+++ Data Structures & Algorithms/generate-parentheses/submission-30.py
@@ -0,0 +1,18 @@
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        result = []
+
+        def dfs(current,open,close):
+            if open == n and close == open:
+                result.append(current)
+                return
+
+            if open < n:
+                dfs(current + "(",open + 1,close)
+            
+            if close < open:
+                dfs(current + ")",open,close + 1)
+            
+
+        dfs("",0,0)
+        return result
